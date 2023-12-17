e7601c94cf55bd9484471498e3ce3e8bd981d361
= <=>
2023-12-17 19:55:21 +0700

    remove hard auth on branch

--- src/notify/service/notify.service.ts
+++ src/notify/service/notify.service.ts
@@ -7,11 +7,8 @@ export class NotifyService {
   constructor(private readonly httpService: HttpService) {}
 
   async notifyJob(branchToken: string, message: string): Promise<any> {
-    branchToken = 'Fd4cZR76hLZF61zvAdhB754UpoL9E5h2EiJyA2IN2ht';
-
     try {
       const headers = {
-        'Content-Type': 'application/x-www-form-urlencoded',
         Authorization: `Bearer ${branchToken}`,
       };
 
@@ -20,7 +17,7 @@ export class NotifyService {
       };
 
       const response = await this.httpService
-        .post('/api/notify', `message=${message}`, { headers })
+        .post('/api/notify', payload, { headers })
         .toPromise();
 
       console.debug('LINE Notify response status:', response.status);
